50b64ce8d9cb2741a3e726675658f74a52a55b2f
Sayan <sayan_saha@hotmail.com>
2025-05-11 11:40:18 -0400

    reroute

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'; // Ensure we use Router with 'as'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router basename="/my-portfolio"> {/* Ensure the basename matches your repository name */}
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );

9c1d2dd067aa17a3c20dcae0104cb58bb7ca27d8
Sayan <sayan_saha@hotmail.com>
2025-05-11 11:49:34 -0400

    using hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom'; // Ensure we use Router with 'as'
+import { HashRouter as Router } from 'react-router-dom';  // Use HashRouter instead of BrowserRouter
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename="/my-portfolio"> {/* Ensure the basename matches your repository name */}
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
